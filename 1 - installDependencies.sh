5d2ecf43ea31150b0022a2255194f2612f1fa871
captain-oski <glogowski.clement@gmail.com>
2023-01-19 22:26:06 -0500

    workshop #1

---
+++ 1 - installDependencies.sh
@@ -0,0 +1,26 @@
+## Mise à jour des librairies OS
+sudo apt-get update &&
+## Installation de VS Code
+    ### Installation des dépendances
+    sudo apt-get install wget gpg -y &&
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg  &&
+    sudo install -D -o root -g root -m 644 packages.microsoft.gpg  /etc/apt/keyrings/packages.microsoft.gpg &&
+    sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list' &&
+    rm -f packages.microsoft.gpg &&
+    sudo apt install apt-transport-https -y &&
+    sudo apt update -y &&
+    ### Installation de VS Code
+    sudo apt install code -y &&
+## Installation de Docker
+    sudo apt-get install curl &&
+    curl -fsSL https://get.docker.com -o get-docker.sh &&
+    sudo sh get-docker.sh &&
+    sudo usermod -a -G docker $USER &&
+
+### Installation de Docker compose
+sudo apt-get install docker-compose-plugin -y &&
+
+### Création du répertoire de travail
+mkdir /home/geo7630/Desktop/geo7630
+
+### Fin de l'installation des dépendances et outils systèmes
--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-install.sh
+# GEO7630 - Intégration et visualisation de données géographiques
+
+## Tutoriel d'installation de la stack d'intégration et de services de données spatiales
+
+`sudo chmod 775 1\ -\ installDependencies.sh && sudo chmod 775 2\ -\ createRepo.sh`
+`./1\ -\ installDependencies.sh`
+`./2\ -\ createRepo.sh`
 
 cd docker && sudo docker compose up -d
 

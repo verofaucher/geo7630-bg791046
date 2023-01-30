9034db8df901c761365b66aa7a93db2f5630c831
captain-oski <glogowski.clement@gmail.com>
2023-01-29 20:53:52 -0500

    Lab 2 beta

---
+++ Laboratoires/Lab2/download_lab_data.sh
@@ -0,0 +1,5 @@
+
+cd data &&
+wget -O Lab2.zip --no-check-certificate "https://drive.google.com/u/1/uc?id=1kTG5apzdr4tNB63o-bS2V2ALfEwjw40P&export=download&confirm=t&uuid=0168979b-96ba-4cfe-b46f-e3166193baaf&at=ALgDtswAAWrpoaz5QK2cRSLWTWeI:1675022891070" && 
+unzip -a Lab2.zip &&
+sudo rm Lab2.zip

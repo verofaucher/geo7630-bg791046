0ac7d19952d667f63524dc012cd00dbc93434df1
Captain-Oski <Captain-Oski@users.noreply.github.com>
2023-03-11 15:14:43 +0000

    Lab6

---
+++ Laboratoires/Lab6/lab6.js
@@ -0,0 +1,6 @@
+var map = new maplibregl.Map({
+    container: 'map',
+    style: 'https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL',
+    center: [12.338, 45.4385],
+    zoom: 17.4
+    });

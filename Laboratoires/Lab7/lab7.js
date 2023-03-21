08d96fed15a700df587779956d62a99a3c417921
captain-oski <glogowski.clement@gmail.com>
2023-03-21 08:31:50 -0400

    Lab7 - sans reponse

---
+++ Laboratoires/Lab7/lab7.js
@@ -0,0 +1,8 @@
+
+var map = new maplibregl.Map({
+    container: 'map',
+    style: 'https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL',
+    center: [-73.55, 45.55],
+    zoom: 9
+});
+

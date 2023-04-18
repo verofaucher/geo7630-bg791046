bad92fc0677baa9de87da6fe3aeddbe255981cfa
Captain-Oski <Captain-Oski@users.noreply.github.com>
2023-04-18 14:09:16 -0400

    Create exam2.js

---
+++ Exam/exam2.js
@@ -0,0 +1,7 @@
+
+    var map = new maplibregl.Map({
+        container: 'map', // container id
+        style: 'https://demotiles.maplibre.org/style.json', // style URL
+        center: [0, 0], // starting position [lng, lat]
+        zoom: 1 // starting zoom
+    });

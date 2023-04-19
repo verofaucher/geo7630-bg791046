f5ac37808252c9b0d3bce6b9fe3a81a5c842faeb
verofaucher <123588799+verofaucher@users.noreply.github.com>
2023-04-19 20:45:03 +0000

    Corrections 19 avril 16h44

--- TPs/TP4/app.js
+++ TPs/TP4/app.js
@@ -1,8 +1,23 @@
 // création de la carte Mapbox GL
 var map = new maplibregl.Map({
-  container: 'carte', // identifiant de l'élément HTML conteneur de la carte
-  style: 'https://api.maptiler.com/maps/streets/style.json?key=JhO9AmIPH59xnAn5GiSj', // URL du style de la carte
-  center: [-73.55, 45.55], // position centrale de la carte
-  zoom: 9, // niveau de zoom initial
-  hash: true // activation du hash pour la gestion de l'historique de la carte
+    container: 'map', // identifiant de l'élément HTML conteneur de la carte
+    style: 'https://api.maptiler.com/maps/streets/style.json?key=JhO9AmIPH59xnAn5GiSj', // URL du style de la carte
+    center: [45,5, -73,5], // position centrale de la carte
+    zoom: 9, // niveau de zoom initial
+    hash: true // activation du hash pour la gestion de l'historique de la carte
 });
+
+//ajout des couches geojson
+map.on("load", () => {
+
+  map.addSource("frenes_abattus", {
+    type: "geojson",
+    data: "https://services6.arcgis.com/133a00biU9FItiqJ/arcgis/rest/services/arbres_abattus_frenes/FeatureServer/0/query?f=pgeojson&where=1=1&outFields=*"
+  });
+  
+  map.addLayer({
+    'id': 'frenes_abattus',
+    'type': 'circle',
+    'source': 'frenes_abattus',
+    });
+  });

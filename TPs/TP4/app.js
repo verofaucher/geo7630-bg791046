bb07e11a586e08fb9c1798ae045121572e6df065
verofaucher <123588799+verofaucher@users.noreply.github.com>
2023-04-21 20:17:18 +0000

    Avancement 21 avril 16h17

--- TPs/TP4/app.js
+++ TPs/TP4/app.js
@@ -9,60 +9,60 @@ var map = new maplibregl.Map({
 
 //ajout des couches geojson
 map.on("load", () => {
-map.addSource("arbres_abattus", {
-  type: "geojson",
-  data: "https://services6.arcgis.com/133a00biU9FItiqJ/arcgis/rest/services/arbres_abattus_3857/FeatureServer/0/query?f=pgeojson&where=1=1&outFields=*",
-  cluster: true,
-  clusterMaxZoom: 14, // Max zoom to cluster points on
-  clusterRadius: 50, // Radius of each cluster when clustering points (defaults to 50)
-});
+  map.addSource("arbres_abattus", {
+    type: "geojson",
+    data: "https://services6.arcgis.com/133a00biU9FItiqJ/arcgis/rest/services/arbres_abattus_3857/FeatureServer/0/query?f=pgeojson&where=1=1&outFields=*",
+    cluster: true,
+    clusterMaxZoom: 14, // Max zoom to cluster points on
+    clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+  });
 
-map.addLayer({
-  id: 'clusters_abattus',
-  type: 'circle',
-  source: 'arbres_abattus',
-  filter: ['has', 'point_count'],
-  paint: {
-    // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
-    // with three steps to implement three types of circles:
-    //   * Blue, 20px circles when point count is less than 100
-    //   * Yellow, 30px circles when point count is between 100 and 750
-    //   * Pink, 40px circles when point count is greater than or equal to 750
-    'circle-color': [
-      'step',
-      ['get', 'point_count'],
-      '#51bbd6',
-      100,
-      '#f1f075',
-      750,
-      '#f28cb1'
-    ],
-    'circle-radius': [
-      'step',
-      ['get', 'point_count'],
-      20,
-      100,
-      30,
-      750,
-      40
-    ]
-  }
-});
+  map.addLayer({
+    id: 'clusters_abattus',
+    type: 'circle',
+    source: 'arbres_abattus',
+    filter: ['has', 'point_count'],
+    paint: {
+      // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+      // with three steps to implement three types of circles:
+      //   * Blue, 20px circles when point count is less than 100
+      //   * Yellow, 30px circles when point count is between 100 and 750
+      //   * Pink, 40px circles when point count is greater than or equal to 750
+      'circle-color': [
+        'step',
+        ['get', 'point_count'],
+        '#51bbd6',
+        100,
+        '#f1f075',
+        750,
+        '#f28cb1'
+      ],
+      'circle-radius': [
+        'step',
+        ['get', 'point_count'],
+        20,
+        100,
+        30,
+        750,
+        40
+      ]
+    }
+  });
 
-map.addSource("frenes_proteges", {
-  type: "geojson",
-  data: "https://services6.arcgis.com/133a00biU9FItiqJ/arcgis/rest/services/frenes_proteges_tp4/FeatureServer/0/query?f=pgeojson&where=1=1&outFields=*",
-  cluster: true,
-  clusterMaxZoom: 14, // Max zoom to cluster points on
-  clusterRadius: 50, // Radius of each cluster when clustering points (defaults to 50)
-});
+  map.addSource("frenes_proteges", {
+    type: "geojson",
+    data: "https://services6.arcgis.com/133a00biU9FItiqJ/arcgis/rest/services/frenes_proteges_tp4/FeatureServer/0/query?f=pgeojson&where=1=1&outFields=*",
+    cluster: true,
+    clusterMaxZoom: 14, // Max zoom to cluster points on
+    clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+  });
 
-map.addLayer({
-  id: 'clusters_proteges',
-  type: 'circle',
-  source: 'frenes_proteges',
-  filter: ['has', 'point_count'],,
-  paint: {
+  map.addLayer({
+    id: 'clusters_proteges',
+    type: 'circle',
+    source: 'frenes_proteges',
+    filter: ['has', 'point_count'],
+    paint: {
   // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
   // with three steps to implement three types of circles:
   //   * Blue, 20px circles when point count is less than 100
